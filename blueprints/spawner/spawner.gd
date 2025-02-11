d90dd3db6d75e116ada7fd8bce309fbd1d66adfb
bohdan.bondarenko <bogdan.bondarenko99@gmail.com>
2025-02-11 22:56:31 +0200

    feat: Add ability to choose color of spawned targets directly from spawner

--- blueprints/spawner/spawner.gd
+++ blueprints/spawner/spawner.gd
@@ -3,6 +3,7 @@ extends Node3D
 @export var target_scene: PackedScene
 @export var spawn_place: Vector3
 @export var spawn_delay: float
+@export var spawn_target_color: Color = Color.RED
 
 func _ready():
 	await get_tree().create_timer(spawn_delay).timeout
@@ -12,4 +13,13 @@ func _on_spawn_timer_timeout() -> void:
 	var target_instance = target_scene.instantiate()
 	get_tree().root.add_child(target_instance)
 	target_instance.global_transform.origin = global_transform.origin + spawn_place
+	var mesh = target_instance.get_node("MeshInstance3D")
+	if mesh:
+		var mat = mesh.material_override
+		if not mat:
+			mat = StandardMaterial3D.new()
+		else:
+			mat = mat.duplicate()
+		mat.albedo_color = spawn_target_color
+		mesh.material_override = mat
 	target_instance.apply_impulse(Vector3(0, 0, 3))
